0b0fb1b28111333b4b1e7f7352b4fe1bb35292fc
Alexander Khoroshilov <khoroshilov.alex@gmail.com>
2023-06-29 19:46:27 -0700

    Swagger was implemented for API Docs

--- routes/company.js
+++ routes/company.js
@@ -4,6 +4,15 @@ const { auth, checkRole } = require('../middleware/auth');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /companies:
+ *  get:
+ *    description: Use to get all companies
+ *    responses:
+ *      '200':
+ *        description: A successful response with array of objects
+ */
 router.get('/', getCompanies);
 router.post('/create', auth, checkRole(['admin', 'company']), createCompany);
 
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./database');
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 require('dotenv').config();
 const { NotFoundError } = require('./errors/httpErrors');
 
@@ -10,6 +12,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'A simple Express Library API',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 // Routes
 const userRoutes = require('./routes/user');
 const companyRoutes = require('./routes/company');
@@ -17,9 +33,15 @@ const companyRoutes = require('./routes/company');
 // Use Routes
 app.use('/user', userRoutes);
 app.use('/companies', companyRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.json({
+		message: 'Welcome to our Copal API',
+		version: 'v1',
+		docsUrl: `${process.env.BASE_URL}:${process.env.PORT}/api-docs`,
+	});
 });
+
 
 app.use((req, res, next) => {
 	next(new NotFoundError('Page not found'));
